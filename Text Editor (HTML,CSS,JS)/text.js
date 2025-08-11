c28a62862e43704c6a7a30c51cb61f8a2acf9c93
Narsimlu-561 <narsimlumudiraj92258@gmail.com>
2025-08-12 03:51:04 +0530

    initial commit

---
+++ Text Editor (HTML,CSS,JS)/text.js
@@ -0,0 +1,8 @@
+let input=document.querySelector('#text');
+
+let p=document.querySelector('p');
+
+input.addEventListener("input",function(){
+  console.log(input.value);
+  p.innerText=input.value;
+});
